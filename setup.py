596402f3a81494bfeef8af41e7055255cb3f9c05
Grampurohit <vijayendra.grampurohit@honeywell.com>
2017-05-17 18:43:59 +0530

    setup.py updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sensor_outlier_detector',
+      version='0.1',
+      description='sensor data outlier in the world',
+      url='http://github.com/vijayendra-g/sensor_outlier_detector',
+      author='vG',
+      author_email='vijayendra.iiith@gmail.com',
+      license='GNU',
+      packages=['sensor_outlier_detector'],
+      zip_safe=False)
